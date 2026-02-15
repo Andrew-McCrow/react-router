f7b8f078a9f2537bca8477a4e2956b49bbc5af10
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-15 15:45:20 +0000

    react router testing

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./styles/App.css";
+import { Link } from "react-router";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile/popeye">Popeye's Profile</Link>
+          </li>
+          <li>
+            <Link to="profile/spinach">Spinach's Profile</Link>
+          </li>
+          <li>
+            <Link to="profile/default">Default Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
